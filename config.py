6e20e56f8a27776e6571f71b026fbe3b36ad5829
annakirillovaqq <168228729+annakirillovaqq@users.noreply.github.com>
2026-04-10 21:48:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,1 @@
-import logging 
-
 LOGGING_ENABLED = True
-
-LOG_CONFIG = {
-    'level': 'INFO',
-    'filename': 'log.log'
-    'format': "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s", 
-    'datefmt'='%H:%M:%S',
-}
