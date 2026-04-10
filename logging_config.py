6e79d29ffeaec07373bdb7308a86722b66103a82
annakirillovaqq <168228729+annakirillovaqq@users.noreply.github.com>
2026-04-10 22:59:28 +0300

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -9,3 +9,5 @@ def logger():
             format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s", 
             datefmt='%H:%M:%S',
         )
+        logging.info('Hello')
+        
