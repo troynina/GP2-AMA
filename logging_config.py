6e7ab607ffeecf1af248fb32c85bde935b1df6d4
annakirillovaqq <168228729+annakirillovaqq@users.noreply.github.com>
2026-04-10 22:36:50 +0300

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -2,10 +2,10 @@ import logging
 from config import LOGGING_ENABLED
 
 def logger():
-if LOGGING_ENABLED:
-    logging.basicConfig(
-        level=logging.INFO, 
-        filename = "log.log", 
-        format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s", 
-        datefmt='%H:%M:%S',
-    )
+    if LOGGING_ENABLED:
+        logging.basicConfig(
+            level=logging.INFO, 
+            filename = "log.log", 
+            format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s", 
+            datefmt='%H:%M:%S',
+        )
